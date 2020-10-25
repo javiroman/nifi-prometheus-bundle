f9b44a7503f3e953e3b6a4895296a57cd145f31c
Javi Roman <jroman.espinar@gmail.com>
2020-10-25 10:34:01 +0100

    working fine with scrape_interval 10s and max_samples_per_send=1000 & batch_send_deadline=5s

--- nifi-prometheus-processors/src/main/java/org/apache/nifi/processors/prometheus/PrometheusRemoteWrite.java
+++ nifi-prometheus-processors/src/main/java/org/apache/nifi/processors/prometheus/PrometheusRemoteWrite.java
@@ -91,6 +91,11 @@ public class PrometheusRemoteWrite extends AbstractProcessor {
             .description("All content that is received is routed to the 'success' relationship")
             .build();
 
+    public static final Relationship REL_FAILURE = new Relationship.Builder()
+            .name("failure")
+            .description("All content that is received is routed to the 'failure' relationship")
+            .build();
+
     private List<PropertyDescriptor> descriptors;
 
     private Set<Relationship> relationships;
@@ -106,6 +111,7 @@ public class PrometheusRemoteWrite extends AbstractProcessor {
 
         final Set<Relationship> relationships = new HashSet<Relationship>();
         relationships.add(REL_SUCCESS);
+        relationships.add(REL_FAILURE);
         this.relationships = Collections.unmodifiableSet(relationships);
     }
 
@@ -119,43 +125,9 @@ public class PrometheusRemoteWrite extends AbstractProcessor {
         return descriptors;
     }
 
-    @OnScheduled
-    public void onScheduled(final ProcessContext context) {
-        getLogger().debug("onScheduled called");
-    }
-
-    @Override
-    public void onTrigger(final ProcessContext context, final ProcessSession session) throws ProcessException {
-        final int port = context.getProperty(REMOTE_WRITE_PORT).asInteger();
-        final String contextPath = context.getProperty(REMOTE_WRITE_CONTEXT).getValue();
-
-        getLogger().debug("onTrigger called");
-        serverEndpoint = new Server(port);
-
-        ContextHandler contextHandler = new ContextHandler();
-        contextHandler.setContextPath(contextPath);
-        contextHandler.setHandler(new PrometheusHandler(session));
-        serverEndpoint.setHandler(contextHandler);
-
-        try {
-            serverEndpoint.start();
-            serverEndpoint.join();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
     @OnUnscheduled
     public void onUnscheduled() throws Exception {
         getLogger().debug("onUnscheduled called");
-        if (serverEndpoint != null) {
-            serverEndpoint.stop();
-        }
-    }
-
-    @OnStopped
-    public void onStopped() throws Exception {
-        getLogger().debug("onStopped called");
         if (serverEndpoint != null) {
             serverEndpoint.stop();
         }
@@ -169,14 +141,38 @@ public class PrometheusRemoteWrite extends AbstractProcessor {
         }
     }
 
+    @Override
+    public void onTrigger(final ProcessContext context, final ProcessSession session) throws ProcessException {
+        final int port = context.getProperty(REMOTE_WRITE_PORT).asInteger();
+        final String contextPath = context.getProperty(REMOTE_WRITE_CONTEXT).getValue();
+
+        getLogger().debug("onTrigger called");
+        serverEndpoint = new Server(port);
+
+        ContextHandler contextHandler = new ContextHandler();
+        contextHandler.setContextPath(contextPath);
+        contextHandler.setHandler(new PrometheusHandler(context, session));
+        serverEndpoint.setHandler(contextHandler);
+
+        try {
+            serverEndpoint.start();
+            serverEndpoint.join();
+        } catch (Exception e) {
+            context.yield();
+            e.printStackTrace();
+        }
+    }
 
     private class PrometheusHandler extends AbstractHandler {
         private final JsonFormat.Printer JSON_PRINTER = JsonFormat.printer();
         private ProcessSession session;
+        private ProcessContext context;
+        private FlowFile flowfile;
 
-        public PrometheusHandler(ProcessSession session) {
+        public PrometheusHandler(ProcessContext context, ProcessSession session) {
             super();
             this.session = session;
+            this.context = context;
         }
 
         @Override
@@ -185,14 +181,14 @@ public class PrometheusRemoteWrite extends AbstractProcessor {
                            HttpServletRequest request,
                            HttpServletResponse response) throws IOException,ServletException {
 
-            // Retrieves the body of the request as binary data: protobuf compressed with snappy.
+            // Uncompress the request on the fly: Snappy compressed protocol buffer message.
             try (SnappyInputStream is = new SnappyInputStream(baseRequest.getInputStream())) {
 
                 if (is == null) {
-                     getLogger().warn("Inputstream is null");
+                     getLogger().error("Inputstream is null");
                 }
 
-                ServletOutputStream out = response.getOutputStream();
+                ServletOutputStream reponseOut = response.getOutputStream();
                 response.setStatus(HttpServletResponse.SC_OK);
 
                 WriteRequest writeRequest = WriteRequest.parseFrom(is);
@@ -203,7 +199,7 @@ public class PrometheusRemoteWrite extends AbstractProcessor {
                     Metrics metrics = new Metrics();
                     Gson gson = new Gson();
 
-                    FlowFile flowFile = session.create();
+                    flowfile = session.create();
                     getLogger().debug("Creating flow file for event: {}.", new Object[]{timeSeries});
 
                     for (Types.Label labelItem: timeSeries.getLabelsList()) {
@@ -220,23 +216,27 @@ public class PrometheusRemoteWrite extends AbstractProcessor {
                     }
                     metrics.metricLabels= labelsList;
                     metrics.metricSamples = sampleList;
-                    flowFile = session.write(flowFile, new OutputStreamCallback() {
+                    getLogger().debug("write content of FlowFile: {}.", new Object[]{flowfile.getId()});
+                    flowfile = session.write(flowfile, new OutputStreamCallback() {
                                 @Override
                                 public void process(OutputStream out) throws IOException {
                                     out.write(gson.toJson(metrics).getBytes());
                                     out.flush();
+                                    out.close();
                                 }
                             });
 
-                    session.putAttribute(flowFile, "mime.type","application/json");
-                    getLogger().debug("sucess relation for flow file: {}.", new Object[]{flowFile});
-                    session.transfer(flowFile, REL_SUCCESS);
-                    session.getProvenanceReporter().receive(flowFile, request.getRequestURI());
+                    session.putAttribute(flowfile, "mime.type","application/json");
+                    getLogger().debug("SUCCESS relation for flow file: {}.", new Object[]{flowfile.getId()});
+                    session.transfer(flowfile, REL_SUCCESS);
+                    session.getProvenanceReporter().receive(flowfile, request.getRequestURI());
                     session.commit();
                 }
-                out.flush();
                 baseRequest.setHandled(true);
             } catch (IOException e) {
+                getLogger().error("Ran into an error while processing {}.", new Object[] {flowfile.getId()}, e);
+                session.transfer(flowfile, REL_FAILURE);
+                context.yield();
                 throw e;
             }
         }
